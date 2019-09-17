9fca1d9d82394bbd65e0853de0120d54fc77ec42
Trevor Nodland <trevor.nodland@gmail.com>
2019-09-17 10:26:32 -0600

    Finishes writing test for getting all olympians

--- koroibos/tests.py
+++ koroibos/tests.py
@@ -32,4 +32,40 @@ class AllOlymipiansTest(BaseTest):
             sport=sport
         )
         olympian_1.save()
-        import code; code.interact(local=dict(globals(), **locals()))
+
+        olympian_2 = Olympian(
+            name='Sport Woman',
+            sex='F',
+            age=25,
+            height=150,
+            weight=200,
+            team='Brazil',
+            games='2016 Summer',
+            event=event,
+            sport=sport,
+            medal='Gold'
+        )
+        olympian_2.save()
+
+        olympian_3 = Olympian(
+            name='Sport Person',
+            sex='U',
+            age=25,
+            height=150,
+            weight=200,
+            team='Germany',
+            games='2016 Summer',
+            event=event,
+            sport=sport
+        )
+        olympian_3.save()
+
+        response = self.client.get('/api/v1/olympians')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['olympians']), 3)
+        self.assertEqual(response.data['olympians'][0]['name'], olympian_1.name)
+        self.assertEqual(response.data['olympians'][0]['age'], olympian_1.age)
+        self.assertEqual(response.data['olympians'][0]['sex'], olympian_1.sex)
+        self.assertEqual(response.data['olympians'][0]['total_medals_won'], 0)
+        self.assertEqual(response.data['olympians'][1]['total_medals_won'], 1)
